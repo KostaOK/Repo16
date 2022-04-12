cad4f40e74bf4b8b2c49cef4f0305ae40811a373
Niko Kostič <kostic.niko@icloud.com>
2022-04-12 21:37:07 +0200

    Defining the Constructor Function w/ class Syntax

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+class Car {
+    constructor(manufacturer, model, production) {
+        this.manufacturer = "BMW";
+        this.model = "BMW X3";
+        this.production = 2003;
+    }
+}
+// Only change code above this line
+const bmw = new Car("BMW", "BMW X3", 2003);
+console.log(bmw.manufacturer);
+console.log(bmw.model);
+console.log(bmw.production);
+module.exports = Car;
